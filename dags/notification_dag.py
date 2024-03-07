b526d26cc3ba111c3d2f20106e211d96b1274d8b
gngsn <enfj.dev@gmail.com>
2024-03-07 12:45:27 +0900

    add: create user model with pydantic

--- dags/notification_dag.py
+++ dags/notification_dag.py
@@ -8,32 +8,29 @@ def generator():
         "id": 0,
         "schedule": "*/30 9-18 * * MON,TUE,WED,THU,FRI",
         "template_id": "UMSV10001",
-        "argument": {
-            "task_count": "select count(*) as task_count from TODO ..."
-        },
-        "argument_valid": [
-            "task_count"
-        ]
+        "argument": {"task_count": "select count(*) as task_count from TODO ..."},
+        "argument_valid": ["task_count"],
     }
 
 
+# virtualenv
 @task
 def send(schema):
     messages = [
         {
             "template_id": "UMSV10001",
             "title": "이번 주 회의 건 수",
-            "content": "이번 주 {{task_count}}개의 회의가 예정되어 있어요."
+            "content": "이번 주 {{task_count}}개의 회의가 예정되어 있어요.",
         },
         {
             "template_id": "UMSV10002",
             "title": "새로운 회의",
-            "content": "새로운 회의가 있습니다.\n 담당자: {{owner}} | 시간: {{time}}"
+            "content": "새로운 회의가 있습니다.\n 담당자: {{owner}} | 시간: {{time}}",
         },
         {
             "template_id": "UMSV10003",
             "title": "회의 시작 10분 전",
-            "content": "{{meeting_name}} 회의가 10분 뒤에 {{location}}에서 실행돼요."
+            "content": "{{meeting_name}} 회의가 10분 뒤에 {{location}}에서 실행돼요.",
         },
     ]
 
