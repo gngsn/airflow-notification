1be490f5a4599c4d4d09891b02c40853fbeb96ae
gngsn <enfj.dev@gmail.com>
2024-03-09 15:42:01 +0900

    feat: modify dict keys to consts

--- dags/notification_dag.py
+++ dags/notification_dag.py
@@ -1,43 +1,14 @@
 import pendulum
+
 from airflow.decorators import dag, task
 
+TEMPLATE_ID = "template_id"
+TEMPLATE_KEY_SCHEDULE = "schedule"
+TEMPLATE_KEY_CHECKSUM = "checksum"
+TEMPLATE_KEY_ARGS = "argument"
 
-@task()
-def generator():
-    return {
-        "id": 0,
-        "schedule": "*/30 9-18 * * MON,TUE,WED,THU,FRI",
-        "template_id": "UMSV10001",
-        "argument": {"task_count": "select count(*) as task_count from TODO ..."},
-        "argument_valid": ["task_count"],
-    }
-
-
-# virtualenv
-@task
-def send(schema):
-    messages = [
-        {
-            "template_id": "UMSV10001",
-            "title": "이번 주 회의 건 수",
-            "content": "이번 주 {{task_count}}개의 회의가 예정되어 있어요.",
-        },
-        {
-            "template_id": "UMSV10002",
-            "title": "새로운 회의",
-            "content": "새로운 회의가 있습니다.\n 담당자: {{owner}} | 시간: {{time}}",
-        },
-        {
-            "template_id": "UMSV10003",
-            "title": "회의 시작 10분 전",
-            "content": "{{meeting_name}} 회의가 10분 뒤에 {{location}}에서 실행돼요.",
-        },
-    ]
-
-    if schema is None:
-        return "Error"
-
-    return [d for d in messages if d["template_id"] == schema["template_id"]]
+MESSAGE_TITLE = "title"
+MESSAGE_CONTENT = "content"
 
 
 @dag(
@@ -46,6 +17,41 @@ def send(schema):
     catchup=False,
 )
 def trigger():
+    @task
+    def generator():
+        return {
+            TEMPLATE_ID: "UMSV10001",
+            TEMPLATE_KEY_SCHEDULE: "*/30 9-18 * * MON,TUE,WED,THU,FRI",
+            TEMPLATE_KEY_ARGS: {"task_count": "select count(*) as task_count from TODO ..."},
+            TEMPLATE_KEY_CHECKSUM: "task_count",
+        }
+
+    # virtualenv
+    @task
+    def send(schema: dict):
+        messages = [
+            {
+                TEMPLATE_ID: "UMSV10001",
+                MESSAGE_TITLE: "이번 주 회의 건 수",
+                MESSAGE_CONTENT: "이번 주 {{task_count}}개의 회의가 예정되어 있어요.",
+            },
+            {
+                TEMPLATE_ID: "UMSV10002",
+                MESSAGE_TITLE: "새로운 회의",
+                MESSAGE_CONTENT: "새로운 회의가 있습니다.\n 담당자: {{owner}} | 시간: {{time}}",
+            },
+            {
+                TEMPLATE_ID: "UMSV10003",
+                MESSAGE_TITLE: "회의 시작 10분 전",
+                MESSAGE_CONTENT: "{{meeting_name}} 회의가 10분 뒤에 {{location}}에서 실행돼요.",
+            },
+        ]
+
+        if schema is None:
+            return "Error"
+
+        return [d for d in messages if d["template_id"] == schema["template_id"]]
+
     schema = generator()
     send(schema)
 
