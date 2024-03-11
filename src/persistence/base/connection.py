53b530b864e87b8e24b6e3062881823a33a90e19
gngsn <enfj.dev@gmail.com>
2024-03-11 23:22:07 +0900

    feat: extend db initializer

--- src/persistence/base/connection.py
+++ src/persistence/base/connection.py
@@ -1,13 +1,23 @@
-__all__ = ['init_db', 'database_', 'execute']
+__all__ = ['init_pg', 'init_sqlite', 'database_', 'execute']
 
-from peewee import PostgresqlDatabase, Proxy, Model
+from peewee import PostgresqlDatabase, Proxy, Model, Database, SqliteDatabase
 
 database_ = Proxy()
 
 
-def init_db(url="postgresql://postgres:postgres@localhost/postgres"):
+def init_db(initializer: callable, db: Database):
     """ Lazy Initialize Database Connection """
-    database_.initialize(PostgresqlDatabase(url))
+    initializer(db)
+
+
+def init_pg(url="postgresql://postgres:postgres@localhost/postgres") -> None:
+    """Initialize PostgreSQL database."""
+    init_db(database_.initialize, PostgresqlDatabase(url))
+
+
+def init_sqlite(url=":memory:") -> None:
+    """Initialize PostgreSQL database."""
+    init_db(database_.initialize, SqliteDatabase(url))
 
 
 def execute(sql):
