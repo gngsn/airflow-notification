5810a5be1f60ed75c6c3f4a95fd2cc7548f49733
gngsn <enfj.dev@gmail.com>
2024-05-10 12:24:25 +0900

    test: add test all dags

---
+++ tests/test_dags.py
@@ -0,0 +1,27 @@
+import os
+
+import pytest
+from airflow.models import DagBag
+
+
+def get_dags():
+    """
+    Generate a tuple of dag_id, <DAG objects> in the DagBag
+    """
+
+    dag_bag = DagBag(include_examples=False)
+
+    def strip_path_prefix(path):
+        return os.path.relpath(path, os.environ.get("AIRFLOW_HOME"))
+
+    return [(k, v, strip_path_prefix(v.fileloc)) for k, v in dag_bag.dags.items()]
+
+
+APPROVED_TAGS = {"", "op_analytics", "product"}
+
+
+@pytest.mark.parametrize(
+    "dag_id,dag,fileloc", get_dags(), ids=[x[2] for x in get_dags()]
+)
+def test_all(dag_id, dag, fileloc):
+    dag.test()
