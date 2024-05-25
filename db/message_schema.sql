7803da896ef427fa812734a7fc283a1040ebb3a6
gngsn <enfj.dev@gmail.com>
2024-05-26 00:25:01 +0900

    feat: modify message schema

--- db/message_schema.sql
+++ db/message_schema.sql
@@ -1,11 +1,12 @@
 CREATE TABLE message_schema
 (
-    id         BIGSERIAL,
-    schedule   VARCHAR(100),
-    args       VARCHAR(1000),
-    target     VARCHAR(1000),
-    checksum   VARCHAR(200),
-    updated_at TIMESTAMP DEFAULT CLOCK_TIMESTAMP(),
+    id            BIGSERIAL,
+    template_id   VARCHAR(50),
+    schedule      VARCHAR(100),
+    args          VARCHAR(1000),
+    target        VARCHAR(1000),
+    checksum_keys VARCHAR(200),
+    updated_at    TIMESTAMP DEFAULT CLOCK_TIMESTAMP(),
     PRIMARY KEY (id)
 );
 
