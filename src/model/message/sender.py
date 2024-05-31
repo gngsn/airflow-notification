77f8a6f56f3ca22b0c6a83f63e71447c7c5fd535
gngsn <enfj.dev@gmail.com>
2024-05-31 09:22:57 +0900

    feat: create Sender structure for multiple Senders

--- src/model/message/sender.py
+++ src/model/message/sender.py
@@ -1,9 +1,33 @@
+from abc import ABC
+
 from src.persistence.queue import NotificationQueue
 
 
 def run():
+    chunk = 10
     dequeue = NotificationQueue.dequeue()
+
+    senders = [ConsoleSender()]
 
     print(dequeue)
     for message in dequeue:
+        for sender in senders:
+            code = sender.send(message)
+
+            if code == -1:
+                print("ERROR")
+            if code == 0:
+                print("Nothing happened")
+            if code == 1:
+                print("SUCCESS")
+
+
+class Sender(ABC):
+    def send(self, message) -> int:
+        raise NotImplementedError()
+
+
+class ConsoleSender(Sender):
+    def send(self, message) -> int:
         print(message)
+        return 1
