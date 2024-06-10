cf93a486913e0480646156bd226615410271a177
gngsn <enfj.dev@gmail.com>
2024-06-11 00:33:11 +0900

    feat: sending messages into chunk

--- src/model/message/sender.py
+++ src/model/message/sender.py
@@ -1,23 +1,30 @@
 from abc import ABC
+from itertools import islice
+from typing import Iterator
 
 from src.persistence.queue import NotificationQueue
 
 
+def into_chuck(it: Iterator, n: int) -> Iterator[list]:
+    while chunk := list(islice(it, n)):
+        yield chunk
+
+
 def run():
-    chunk = 10
-    dequeue = NotificationQueue.dequeue(chunk)
+    chunk = 100
     senders = [ConsoleSender()]
 
-    for message in dequeue:
-        for sender in senders:
+    dequeue = NotificationQueue.dequeue(chunk)
+    for sender in senders:
+        for message in into_chuck(dequeue, chunk):
             code = sender.send(message)
 
-            if code == -1:
-                print("ERROR")
-            if code == 0:
-                print("Nothing happened")
-            if code == 1:
-                print("SUCCESS")
+        if code == -1:
+            print("ERROR")
+        if code == 0:
+            print("Nothing happened")
+        if code == 1:
+            print("SUCCESS")
 
 
 class Sender(ABC):
