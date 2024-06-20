b6bfe90470f9d869bd808ced743ce444076fe812
gngsn <enfj.dev@gmail.com>
2024-06-20 23:37:16 +0900

    feat: make Connector to be compatibility with python resource auto close

--- src/persistence/base/connection.py
+++ src/persistence/base/connection.py
@@ -1,6 +1,6 @@
 __all__ = ['init_pg', 'init_sqlite', 'database_', 'execute', 'BaseModel']
 
-from peewee import PostgresqlDatabase, Model, SqliteDatabase, Proxy
+from peewee import PostgresqlDatabase, Model, SqliteDatabase, Proxy, Database
 
 database_ = Proxy()
 
@@ -25,15 +25,26 @@ class BaseModel(Model):
 
 
 class Connector:
-    host: str
-    db_name: str
-    db_schema: str
-    username: str
-    password: str
+    __property__: dict
+    __connection__: Database = None
 
-    def __init__(self, host: str, db_name: str, db_schema: str, username: str, password: str):
-        self.host = host
-        self.db_name = db_name
-        self.db_schema = db_schema
-        self.username = username
-        self.password = password
+    def __init__(self, host: str, db_name: str, db_port: int, username: str, password: str):
+        self.__property__ = {
+            'db_name': db_name,
+            'user': username,
+            'password': password,
+            'host': host,
+            'port': db_port
+        }
+
+    def __enter__(self):
+        self.__connection__ = PostgresqlDatabase(
+            self.__property__['db_name'],
+            user=self.__property__['user'],
+            password=self.__property__['password'],
+            host=self.__property__['host'],
+            port=self.__property__['db_port']
+        )
+
+    def __exit__(self):
+        self.__connection__.close()
