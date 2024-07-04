4a084d5ed43e671cd910bb86bc7334936abaa505
gngsn <enfj.dev@gmail.com>
2024-07-05 00:15:53 +0900

    test: add singleton decorator test

---
+++ tests/test_singleton.py
@@ -0,0 +1,32 @@
+from src.lib.decorator.singleton import singleton
+
+
+@singleton
+class SingletonPrinter:
+    """
+        Singleton
+        Hedwig 메세지 전송 담당 Hook - Formatting / Calling API
+    """
+
+    def __new__(cls, *args, **kwargs):
+        print("__new__ is called\n")
+        instance = super().__new__(cls)
+        return instance
+
+    def __init__(self, name):
+        self.name = name
+        print("__init__ is called\n")
+
+    def print(self):
+        print(f"Hi hi I'm {self.name}")
+
+
+def test_singleton():
+    printers = [
+        SingletonPrinter("싱글톤 테스트 1"),
+        SingletonPrinter("싱글톤 테스트 2"),
+        SingletonPrinter("싱글톤 테스트 3")
+    ]
+
+    for p in printers:
+        print(p.print())
