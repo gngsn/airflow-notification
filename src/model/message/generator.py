4368ec2e3fcece888c826bf3512b52c2fe856eeb
gngsn <enfj.dev@gmail.com>
2024-07-24 23:45:39 +0900

    fix: modify Connector to be running

--- src/model/message/generator.py
+++ src/model/message/generator.py
@@ -27,7 +27,6 @@ def run(_setup=_init):
 def _generate():
     for schema in MessageSchema.select_all():
         connection = ExternalDbConnection.get_target_db_connection(schema)
-        print(f"connection: {connection}, type: {type(connection)}")
 
         with Connector(connection):
             template_id = schema.template_id
@@ -38,32 +37,42 @@ def _generate():
                 continue
 
             for args in _get_targets(schema):
-                formed = replace(template.message, args)
+                for users in _get_target_users(schema, args):
+                    args = args | users
+                    target_id = str(args["target"])
 
-                target_id = args["target"]
+                    formed = replace(template.message, args)
+                    message = Message(title=template.title, body=formed, to=target_id)
+                    notification_checksums = [args[key] for key in schema.checksum_keys.split(",")]
 
-                message = Message(title=template.title, body=formed, to=target_id)
-                notification_checksums = [args[key] for key in schema.checksum_keys.split(",")]
-
-                queue_checksum = _make_checksum(schema.template_id, target_id, notification_checksums)
-                NotificationQueue.enqueue(queue_checksum, message.__dict__())
+                    queue_checksum = _make_checksum(schema.template_id, target_id, notification_checksums)
+                    NotificationQueue.enqueue(queue_checksum, message.dict())
 
 
 def _make_checksum(template_id: str, target: str, checksums: list[str]):
-    return ":".join([template_id, target, checksums])
+    return ":".join([template_id, target] + checksums)
+
+
+def row_to_json(cursor):
+    """ Convert retrieved database rows to json """
+    for row in cursor.fetchall():
+        yield {column[0]: str(value) for column, value in zip(cursor.description, row)}
 
 
 @transactional
 def _get_targets(schema: MessageSchema) -> Generator:
-    """ Get targets by executing schema's target properties which is a sql """
-
-    def row_to_json(cursor):
-        """ Convert retrieved database rows to json """
-        for row in cursor.fetchall():
-            yield {column[0]: value for column, value in zip(cursor.description, row)}
+    """ Get targets by executing schema's target properties which is a sql query """
 
     targets = replace(schema.get_targets(), json.loads(schema.args))
     cursor = execute(targets)
+    return row_to_json(cursor)
+
+
+def _get_target_users(schema: MessageSchema, args: dict) -> Generator:
+    """ Get users to be sent by executing target_users properties which is a sql query """
+
+    users = replace(schema.target_users, args | json.loads(schema.args))
+    cursor = execute(users)
     return row_to_json(cursor)
 
 
--- src/model/message/message.py
+++ src/model/message/message.py
@@ -5,6 +5,3 @@ class Message(BaseModel):
     title: str
     body: str
     to: str
-
-    def __dict__(self) -> dict:
-        return self.model_dump()
